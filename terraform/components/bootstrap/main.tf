6220a79bc06cf53483a6864e9a1a27f2e6d2b12e
Lewis Cowles <lewiscowles@me.com>
2021-04-22 09:53:43 +0100

    Update main.tf

--- terraform/components/bootstrap/main.tf
+++ terraform/components/bootstrap/main.tf
@@ -1,8 +1,14 @@
 locals {
-  domain                = "techlete.co.uk"
+  co_uk_domain          = "techlete.co.uk"
+  uk_domain             = "techlete.uk"
 }
 
 module "route53_base" {
   source                = "../../modules/AwsDnsZoneRecord/"
-  zone_name             = "${local.domain}"
+  zone_name             = "${local.co_uk_domain}"
+}
+
+module "route53_uk" {
+  source                = "../../modules/AwsDnsZoneRecord/"
+  zone_name             = "${local.uk_domain}"
 }
